ecc3ee7a217fdfef2d09706cb3e1f0286d1cd152
joe <joecc@umich.edu>
2021-02-08 13:55:29 -0500

    yolomarker starts

--- yolomarker.py
+++ yolomarker.py
@@ -1,23 +1,17 @@
-from os import listdir
-from os import path
+from os import listdir, path
 from shutil import copyfile
 
 def filter():
-    label_path = "/home/cchen_backup/Desktop/Filtered_dataset/labels"
-    images_path = "/home/cchen_backup/Desktop/Filtered_dataset/images"
-    new_label_path = "/home/cchen_backup/Desktop/Filtered_dataset/new_labels"
+    desktop = path.join(path.join(path.expanduser('~')), 'Desktop') 
+    label_path = desktop+"/Filtered_dataset/labels"
+    images_path = desktop+"/Filtered_dataset/images"
+    new_label_path = desktop+"/Filtered_dataset/new_labels"
 
-    for f in listdir(label_path):
-        with open(path.join(label_path,f), "r") as file:
-            with open(path.join(new_label_path,f), "w") as output: 
-                for line in file:
-                    if int(line[0]) == 0:
-                        output.write(line)
-                    elif int(line[0]) == 1:
-                        output.write(line)
-
-
-                    
+    for f in listdir(images_path):
+        new_label = f.split(".")[0] + ".txt"
+        print("new_label::::::::::"+str(new_label))
+        with open(path.join(new_label_path,f), "w") as output: 
+            pass
 
 if __name__ == '__main__':
     filter()
