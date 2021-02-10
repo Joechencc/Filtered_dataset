e7367dd5add5bb6b5f422d4ef306777e7682c1a4
cc <joecc@umich.edu>
2021-02-09 23:26:56 -0500

    new_visual

---
+++ visualize.py
@@ -0,0 +1,33 @@
+from os import listdir, path
+import cv2
+import os
+import numpy as np
+
+def visualize():
+    desktop = path.join(path.join(path.expanduser('~')), 'Desktop') 
+    images_path = desktop+"/Filtered_dataset/scaled_images_backup"
+    new_label_path = desktop+"/Filtered_dataset/new_labels"
+    image_p =path.join(images_path,"DOR_S3_21.JPG")
+    image = cv2.imread(image_p)
+    height = image.shape[0]
+    width = image.shape[1]
+    with open(path.join(new_label_path,"DOR_S3_21.txt")) as fp:
+        line = fp.readline()
+        while line:
+            x = line.split(" ")[1]
+            y = line.split(" ")[2]
+            wid = line.split(" ")[3]
+            hgt = line.split(" ")[4]
+
+            #print("line::"+str(line.split(" ")))
+            cv2.rectangle(image, (int(float(x)*width),int(float(y)*height)), (int(float(x)*width+float(wid)*width),int(float(y)*height+float(hgt)*height)), (255,0,0), -1)
+            line = fp.readline()
+
+    cv2.namedWindow("image")
+    cv2.imshow("image", image)
+    cv2.waitKey(30000) & 0xFF
+
+    
+if __name__ == '__main__':
+    visualize()
+
